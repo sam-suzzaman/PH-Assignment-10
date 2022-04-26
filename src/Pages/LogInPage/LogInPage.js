ca024a16ad1f4645e7bbac4d37067018f2808d0e
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-04-26 22:26:20 +0600

    adding login functionality

--- src/Pages/LogInPage/LogInPage.js
+++ src/Pages/LogInPage/LogInPage.js
@@ -1,23 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./LogInPage.css";
+import {
+    useSignInWithEmailAndPassword,
+    useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+
+import firebaseAuth from "../../firebase.init";
 
 const LogInPage = () => {
+    const [signInWithEmailAndPassword, user, loading, error] =
+        useSignInWithEmailAndPassword(firebaseAuth);
+    const [signInWithGoogle, googleUser, googleLoading, googleError] =
+        useSignInWithGoogle(firebaseAuth);
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [remember, setRemember] = useState(false);
+
+    // errors
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+
+    const handleLogInSubmit = (e) => {
+        e.preventDefault();
+        if (!/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(email)) {
+            setEmailError(true);
+            setPasswordError(false);
+        } else if (password.length < 6) {
+            setEmailError(false);
+            setPasswordError(true);
+        } else {
+            signInWithEmailAndPassword(email, password);
+            setEmail("");
+            setPassword("");
+            setRemember(false);
+            // for error
+            setEmailError(false);
+            setPasswordError(false);
+        }
+    };
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle();
+    };
+
     return (
         <div className="form-container">
+            <h4
+                style={{
+                    textAlign: "center",
+                    fontSize: "0.8rem",
+                    color: "red",
+                    marginBottom: "1rem",
+                }}
+            >
+                {error?.message || googleError?.message}
+            </h4>
             <h3 className="form-title">login form</h3>
-            <form>
+            <form onSubmit={handleLogInSubmit}>
                 <div className="input-control">
                     <label htmlFor="email">enter Your email:</label>
-                    <input type="email" name="email" id="" />
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        name="email"
+                        id="email"
+                    />
+                    {emailError && <p className="error">enter a valid email</p>}
                 </div>
                 <div className="input-control">
                     <label htmlFor="password">enter Your password:</label>
-                    <input type="password" name="password" id="" />
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        name="password"
+                        id="password"
+                    />
+                    {passwordError && (
+                        <p className="error">Password length be atleast 6</p>
+                    )}
                 </div>
                 <div className="form-info-row">
                     <div className="check-box">
-                        <input type="checkbox" name="checkbox" id="checkbox" />
+                        <input
+                            type="checkbox"
+                            checked={remember}
+                            onChange={(e) => setRemember(e.target.checked)}
+                            name="checkbox"
+                            id="checkbox"
+                        />
                         <label htmlFor="checkbox">remember me</label>
                     </div>
                     <p className="forget-password">forget password</p>
@@ -36,16 +110,16 @@ const LogInPage = () => {
                     <div className="divider-right divider"></div>
                 </div>
                 <div className="social-login-container">
-                    <button>
-                        <i class="fa-brands fa-google"></i>{" "}
+                    <button onClick={handleGoogleSignIn}>
+                        <i className="fa-brands fa-google"></i>{" "}
                         <span className="btn-text">google</span>
                     </button>
                     {/* <button>
-                        <i class="fa-brands fa-github"></i>{" "}
+                        <i className="fa-brands fa-github"></i>{" "}
                         <span className="btn-text">github</span>
                     </button>
                     <button>
-                        <i class="fa-brands fa-facebook"></i>{" "}
+                        <i className="fa-brands fa-facebook"></i>{" "}
                         <span className="btn-text">facebook</span>
                     </button> */}
                 </div>
