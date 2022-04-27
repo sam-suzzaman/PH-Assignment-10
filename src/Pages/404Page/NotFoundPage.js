219d366de8a3f0ed4ee7d5fbd2b9cecf216663fb
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-04-27 13:36:21 +0600

    adding 404 page

--- src/Pages/404Page/NotFoundPage.js
+++ src/Pages/404Page/NotFoundPage.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NotFound.css";
 
 const NotFoundPage = () => {
-    return <div>this is not found page</div>;
+    return (
+        <section className="error-container">
+            <h2 className="error-title">Sorry! 404 - page not found</h2>
+            <Link to="/" className="error-btn">
+                go to home
+            </Link>
+        </section>
+    );
 };
 
 export default NotFoundPage;
