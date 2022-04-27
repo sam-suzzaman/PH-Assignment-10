4d61465034e173a7a31773431af9c93bda9a4780
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-04-27 11:50:03 +0600

    adding private route

--- src/utilities/RequireRoute.js
+++ src/utilities/RequireRoute.js
@@ -1,18 +1,23 @@
-import { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
 import firebaseAuth from "./../firebase.init";
 
 function RequireAuth({ children }) {
     let location = useLocation();
-    const [userInfo, setUserInfo] = useState({});
-    const [user] = useAuthState(firebaseAuth);
+    const [user, loading] = useAuthState(firebaseAuth);
 
-    useEffect(() => {
-        setUserInfo(user);
-    }, [user]);
-
-    if (!userInfo) {
+    if (loading) {
+        return (
+            <div
+                style={{ height: "100vh" }}
+                className="w-100 d-flex justify-content-center align-items-center"
+            >
+                <Spinner animation="grow" variant="info" />
+            </div>
+        );
+    }
+    if (!user) {
         return <Navigate to="/signIn" state={{ from: location }} replace />;
     }
 
